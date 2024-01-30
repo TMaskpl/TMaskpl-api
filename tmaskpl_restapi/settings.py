aa182fc615ce7f26da9f1ae17a447df512ebb847
Daniel Niemczok <biuro@tmask.pl>
2024-01-30 14:03:02 +0100

    tmaskpl-pgdata

--- tmaskpl_restapi/settings.py
+++ tmaskpl_restapi/settings.py
@@ -66,6 +66,8 @@ TEMPLATES = [
 ]
 
 REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 10,
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication",
     ],
